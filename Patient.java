8bd2c464138214f01dd77be4cc98828196ef03d1
zumraaydin <164158912+zumraaydin@users.noreply.github.com>
2025-12-01 22:09:52 +0300

    Add files via upload

---
+++ Patient.java
@@ -0,0 +1,21 @@
+public class Patient {
+
+
+    // I created some ıdentification for wanted things
+
+    String name;
+    int age;
+    int id;
+    int severity;
+
+    //there is Patient constructor
+
+    public Patient(String name, int age, int id ,int severity ){
+        this.name=name;
+        this.age=age;
+        this.id=id;
+        this.severity=severity;
+
+
+    }
+}
