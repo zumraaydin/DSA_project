7c200b77b02749e34be9df6481b236665d4f8a77
zumraaydin <164158912+zumraaydin@users.noreply.github.com>
2025-12-01 22:13:11 +0300

    task1

---
+++ Node.java
@@ -0,0 +1,13 @@
+public class Node {
+    Patient data;
+    Node next;
+ // here I have special node class for my project
+    public Node (Patient p){
+        this.data=p;
+        this.next=null;
+
+    }
+
+
+
+}
