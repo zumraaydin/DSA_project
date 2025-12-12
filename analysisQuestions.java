432d3d3e6820827ef3c8d219a06716ce71702b96
zumraaydin <164158912+zumraaydin@users.noreply.github.com>
2025-12-12 23:54:02 +0300

    Add files via upload

---
+++ analysisQuestions.java
@@ -0,0 +1,42 @@
+/*
+addPatient(Patient p) bigO is O(n) because;
+if list is empty direct adding O(1),
+and if list is not empty it goes on the last point 0(n).
+
+
+
+removePatient(int id) Big O is O (n) in the  worst situation because of that;
+it keeps going through entire list until it deletes it.
+if it first 0(1),
+if it last 0(n).
+
+
+
+findPatient(int id) BigO is O(n) because ;
+if  patient in the final of list keeps going entire list .
+
+
+
+Compare the performance of Linked List vs. ArrayList fot those operations.
+
+arraylist is faster than linkedlist about finding or searching.
+
+linked list advantage about middle adding or deleting.
+
+In our patient scenario, choosing LinkedIn makes sense because the list is constantly changing.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ */
