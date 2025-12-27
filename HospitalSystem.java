564e77032c6eb0f0139420b1ee42e192a36d56a1
zumraaydin <164158912+zumraaydin@users.noreply.github.com>
2025-12-27 17:02:04 +0300

    Add files via upload

--- HospitalSystem.java
+++ HospitalSystem.java
@@ -1,13 +1,15 @@
 
 import java.util.HashMap;
 
-    public class HospitalSystem {
 
-        PatientList patientList;
-        TreatmentQueue normalQueue;
-        TreatmentQueue priorityQueue;
-        DischargeStack dischargeStack;
-        HashMap<Integer, Patient> patientMap;
+
+   /* public class HospitalSystem {
+
+      private final  PatientList patientList;
+        private final TreatmentQueue normalQueue;
+        private final TreatmentQueue priorityQueue;
+        private final DischargeStack dischargeStack;
+        private final  HashMap<Integer, Patient> patientMap;
 
         public HospitalSystem() {
             patientList = new PatientList();
@@ -34,8 +36,10 @@ import java.util.HashMap;
             }
         }
 
+
+
         // if there is a important it is first
-        public void processTreatment() {
+        public Patient processTreatment() {
 
             TreatmentRequest req;
 
@@ -47,19 +51,21 @@ import java.util.HashMap;
 
             if (req == null) {
                 System.out.println("No treatment requests.");
-                return;
+             
             }
 
-            // bye bye patient
+         /*
             dischargeStack.push(
                     new DischargeRecord(req.patientId, System.currentTimeMillis())
-            );
+            );*/
+          /*  DischargeRecord dr = new DischargeRecord(req.patientId);
+            dischargeStack.push(dr);
+            return  patientMap.get(req.patientId);} */
 
-            patientList.removePatient(req.patientId);
-            patientMap.remove(req.patientId);
-        }
+            //patientList.removePatient(req.patientId);
+            //patientMap.remove(req.patientId);
 
-        // final situation of system
+       /* // final situation of system
         public void printSystemState() {
             System.out.println("---- Current Patients ----");
             patientList.printList();
@@ -72,18 +78,18 @@ import java.util.HashMap;
 
             System.out.println("\n---- Discharge Stack ----");
             dischargeStack.printStack();
-        }
+        } */
 
 
 
-        public void sortPatientsBySeverity() {
+/*        public void sortPatientsBySeverity() {
 
-            if (patientList.head == null) return;
+                if (PatientList.head == null) return;
 
-            boolean swapped;
-            do {
-                swapped = false;
-                Node current = patientList.head;
+                boolean swapped;
+                do {
+                    swapped = false;
+                    Node current = PatientList.head;
 
                 while (current.next != null) {
                     if (current.data.severity < current.next.data.severity) {
@@ -99,6 +105,122 @@ import java.util.HashMap;
             } while (swapped);
         }
 
+        public void addDischargeRecord(int i) {
+        }
+
+        public void processTreatmentRequest() {
+        }
+
+        public void sortPatientsBySeverityDesc() {
+        }
+
+        // final situation of system
+        public void printSystemState() {
+            System.out.println("---- Current Patients ----");
+            PatientList.printList();
+
+            /*System.out.println("\n---- Priority Queue ----");
+            PriorityQueue.printQueue();*/
+          /*  System.out.println("\nTreatment Queue (Priority then Normal)");       //display request in  the priority queueu
+            TreatmentQueue.printQueue();
+
+
+            System.out.println("\n---- Normal Queue ----");
+            PriorityTreatmentQueue normalQueue = null;
+            normalQueue.printQueue();
+
+            System.out.println("\n---- Discharge Stack ----");
+            DischargeStack.printStack();
+        }
+        } */
+
+
+
+
+public class HospitalSystem {
+    PatientList patientList;
+    TreatmentQueue treatmentQueue;
+    DischargeStack dischargeStack;
+    java.util.HashMap<Integer, Patient> patientMap;
+
+    public HospitalSystem() {
+        patientList = new PatientList();
+        treatmentQueue = new TreatmentQueue();
+        dischargeStack = new DischargeStack();
+        patientMap = new java.util.HashMap<Integer, Patient>();
     }
+
+    public void addPatient(Patient patient) {
+        patientList.addPatient(patient);
+        patientMap.put(patient.id, patient);
+    }
+
+    public void addTreatmentRequest(int patientId, boolean priority) {
+        treatmentQueue.enqueue(new TreatmentRequest(patientId, priority));    //for priority queue
+    }
+
+    public void addDischargeRecord(int patientId) {
+        dischargeStack.push(new DischargeRecord(patientId,System.currentTimeMillis()));     //pusheses  new records to the stack
+    }
+
+    public void processTreatmentRequest() {
+        TreatmentRequest request = treatmentQueue.dequeue();       //priority patient first
+        if(request == null) {
+            System.out.println("No treatment request to process.");
+            return;
+        }
+        System.out.println("Processing treatment for patient: " + request.patientId + "(priority: " + request.priority +  ")");   //display the patient
+        dischargeStack.push(new DischargeRecord(request.patientId,System.currentTimeMillis()));
+    }
+
+    public void sortPatientsBySeverityDesc() {
+        Patient[] arr = toArrayFromMap();      //sorts their severity levels that convert map values to array
+        if(arr == null) {
+            return;
+        }
+        for(int i = 0; i < arr.length-1; i++) {
+            for(int j = 0; j< arr.length-1; j++) {
+                if(arr[j].severity < arr[j+1].severity) {
+                    Patient temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+        System.out.println("\nPatient Sorted By Severity (Descending): ");    //show the sorted way
+        for(int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    private Patient[] toArrayFromMap() {
+        if(patientMap.size() == 0) {
+            return null;      //no patient in the system
+        }
+        Patient[] arr = new Patient[patientMap.size()];      //initialize array with same size with map
+        int idx =0;
+        for(Integer key: patientMap.keySet()) {
+            arr[idx] = patientMap.get(key);
+            idx++;
+        }
+        return arr;
+    }
+
+    public void printSystemState() {
+        System.out.println("\n CURRENT SYSTEM STATE");
+
+        System.out.println("\nPatient List (LinkedList) ");      //display stored patient in the linkedlist
+        patientList.printList();
+
+        System.out.println("\nTreatment Queue (Priority then Normal)");       //display request in  the priority queueu
+        treatmentQueue.printQueue();
+
+        System.out.println("\nDischarge Stack (LIFO)");      //display discharge in the stack
+        dischargeStack.printStack();
+    }}
+
+
+
+
 
 
--- Task4Main.java
+++ Task4Main.java
@@ -1,7 +1,60 @@
 
     public class Task4Main {
 
-        public static void main(String[] args) {
+            public static void main(String[] args) {
+                HospitalSystem line4 = new HospitalSystem();
+
+
+                //adding patients
+                line4.addPatient(new Patient("Berra",81,5,5));
+                line4.addPatient(new Patient("Buse",90,9,3));
+                line4.addPatient(new Patient("Züheyla",33,10,1));
+                line4.addPatient(new Patient("Birceakalay",41,11,4));
+                line4.addPatient(new Patient("Cerennaz",40,12,7));
+                line4.addPatient(new Patient("Hayri",22,13,4));
+                line4.addPatient(new Patient("Murat",15,14,8));
+                line4.addPatient(new Patient("ArdaCan",22,15,9));
+                line4.addPatient(new Patient("Furkan",21,16,7));
+                line4.addPatient(new Patient("Serra",20,17,9));
+
+
+                //adding normal
+                //adding normal
+                line4.addTreatmentRequest(14,false);
+                line4.addTreatmentRequest(15,false);
+                line4.addTreatmentRequest(16,false);
+                line4.addTreatmentRequest(12,false);
+                line4.addTreatmentRequest(13,false);
+
+
+                //adding priority
+                line4.addTreatmentRequest(9,true);
+                line4.addTreatmentRequest(10,true);
+                line4.addTreatmentRequest(11,true);
+
+
+                //adding discharge
+                line4.addDischargeRecord(5);
+                line4.addDischargeRecord(9);
+
+
+                //process treatment
+                line4.processTreatmentRequest();
+                line4.processTreatmentRequest();
+
+
+                //sorting by severity
+                line4.sortPatientsBySeverityDesc();
+
+
+                //print final state
+                line4.printSystemState();
+            }
+
+
+        }
+
+       /* public static void main(String[] args) {
 
             HospitalSystem system = new HospitalSystem();
 
@@ -13,7 +66,7 @@
             }
 
             // 5 normal, 3 priority
-            system.addTreatmentRequest(1, false);
+            system.addTreatmentRequest(230444078, false);
             system.addTreatmentRequest(2, false);
             system.addTreatmentRequest(3, false);
             system.addTreatmentRequest(4, false);
@@ -35,7 +88,7 @@
 
             // Son durum
             system.printSystemState();
-        }
-    }
+        } */
+
 
 
